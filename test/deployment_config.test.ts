69e1e47b3220a37f7ea4f28ae50ae6b87abd1ff6
Konstantin Borisov <konstantin.borisov@toolsforhumanity.com>
2024-01-04 11:29:47 -0500

    Initial commit

---
+++ test/deployment_config.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DeploymentConfig from '../lib/deployment_config-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/deployment_config-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DeploymentConfig.DeploymentConfigStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
