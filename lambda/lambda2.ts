d1915ae458ab14308185d3ac3507ebe9dd717f8b
Konstantin Borisov <konstantin.borisov@toolsforhumanity.com>
2024-01-04 12:58:50 -0500

    canary deployment

--- lambda/lambda2.ts
+++ lambda/lambda2.ts
@@ -3,7 +3,7 @@ exports.handler = async function(event: any) {
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
-      body: `Hello, CDK! You've hit ${event.path}\n`
+      body: `Hello, CDK v2! You've hit ${event.path}\n`
     };
   };
   
--- lib/deployment_config-stack.ts
+++ lib/deployment_config-stack.ts
@@ -1,20 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_lambda as lambda, aws_codedeploy, aws_cloudwatch } from 'aws-cdk-lib';
 
 export class DeploymentConfigStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambda1 = new cdk.aws_lambda.Function(this, 'Lambda1', {
-      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
-      code: cdk.aws_lambda.Code.fromAsset('lambda'),
+    const lambda1 = new lambda.Function(this, 'Lambda1', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
       handler: 'lambda1.handler'
     });
 
-    const lambda2 = new cdk.aws_lambda.Function(this, 'Lambda2', {
-      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
-      code: cdk.aws_lambda.Code.fromAsset('lambda'),
+    const lambda2 = new lambda.Function(this, 'Lambda2', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
       handler: 'lambda2.handler'
     });
+
+    // Sequential deployment
+    // lambda2.node.addDependency(lambda1);
+
+    this.configureDeployment({
+      lambda: lambda1,
+      deploymentConfig: aws_codedeploy.LambdaDeploymentConfig.ALL_AT_ONCE,
+      deploymentAlarmErrorsThreshold: 1,
+    });
+
+    this.configureDeployment({
+      lambda: lambda2,
+      deploymentConfig: aws_codedeploy.LambdaDeploymentConfig.ALL_AT_ONCE,//LINEAR_10PERCENT_EVERY_1MINUTE,
+      deploymentAlarmErrorsThreshold: 1,
+    });
+  }
+
+  configureDeployment(params: {
+    lambda: lambda.Function,
+    deploymentConfig?: aws_codedeploy.ILambdaDeploymentConfig,
+    deploymentGroupName?: string,
+    deploymentAlarmErrorsThreshold?: number
+  }): lambda.QualifiedFunctionBase {
+    const newVersion = params.lambda.currentVersion;
+    newVersion.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+    const alias = new lambda.Alias(params.lambda, 'CanaryAlias', {
+      aliasName: 'live',
+      version: newVersion
+    });
+
+    new aws_codedeploy.LambdaDeploymentGroup(params.lambda, 'LambdaDeploymentGroup', {
+      alias,
+      deploymentConfig: params.deploymentConfig,
+      ...(params.deploymentGroupName ? { deploymentGroupName: params.deploymentGroupName } : {}),
+
+      alarms: [
+        new aws_cloudwatch.Alarm(params.lambda, 'DeploymentAlarm', {
+          metric: alias.metricErrors({ period: cdk.Duration.minutes(1) }),
+          threshold: params.deploymentAlarmErrorsThreshold || 10,
+          alarmDescription: `${params.lambda.functionName} ${newVersion.version} canary deployment failure alarm`,
+          evaluationPeriods: 1,
+        }),
+      ],
+    });
+
+    return alias;
   }
 }
